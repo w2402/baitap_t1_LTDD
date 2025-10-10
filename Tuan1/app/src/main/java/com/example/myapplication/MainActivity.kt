fb642c2ea61fd0c52f72eb7d567f679ca0318ac8
Tran Ngoc Dong <dt643919@gmail.com>
2025-10-11 00:12:52 +0700

    Thêm nội dung tuần 1

---
+++ Tuan1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout)
+
+        //bo góc
+        Glide.with(this)
+            .load(R.drawable.hinh)
+            .circleCrop()
+            .into(findViewById(R.id.imageView3))
+    }
+}
+
+
